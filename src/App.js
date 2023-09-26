b42362169739b3b6b89b66bc4e0909d7b902819e
Aleksandar Nedelkovski <aleksn.dev@gmail.com>
2023-09-26 16:59:56 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import "./styles.css";
+import BackgroundAsImageHero from "./Hero";
+
+export default function App() {
+  return (
+    <div className="App">
+      <BackgroundAsImageHero />
+
+      <p className="no">Navigation</p>
+
+      {/* <ul>
+        <li>The Syndicate</li>
+        <li>News from the Void</li>
+        <li>Our Missions</li>
+      </ul> */}
+      <p className="no">News Section</p>
+      <p className="no">Events Calendar</p>
+      <p className="no">About IPTS</p>
+      {/* 
+      <ul>
+        <li>About Us: Guardians of Cosmic Mysteries</li>
+        <li>History Shrouded in Nebulous Paths</li>
+        <li>The Veiled Mission</li>
+        <li>Unearth the Unknown</li>
+        <li>Join the Enigma</li>
+      </ul> */}
+
+      <p className="no">Services or Offerings</p>
+      <p className="no">Contact Information</p>
+      <p className="no">Media Gallery</p>
+      <p className="no">Newsletter Signup</p>
+      <p className="no">Privacy and Security</p>
+      <p className="no">Dark Mode</p>
+      <p className="no">Interactive Elements</p>
+      <p className="no">Cryptic Message Board</p>
+      <p className="no">Limited Access Areas</p>
+    </div>
+  );
+}
